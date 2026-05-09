7709bb25c300d7ef8e5334c86c195b1c4e2103e5
joe-coyi <west.ham@vuultweb.co.uk>
2026-05-09 14:44:15 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
-import { nitro } from 'nitro/vite';
+import nitro from 'nitropack';
 
 export default defineConfig({
   plugins: [tanstackStart(), nitro(), viteReact()],
